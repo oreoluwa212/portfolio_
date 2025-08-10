4240a45112b00d1798dc6e1c9a46fb137b3adcfe
oreoluwa212 <oreoluwaajayyiruth@gmail.com>
2025-08-10 20:01:37 +0100

    fix: quick fix

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -4,7 +4,7 @@ import web1 from "../public/web1.png";
 import web2 from "../public/web2.png";
 import web9 from "../public/web9.png";
 import web4 from "../public/web4.png";
-import web5 from "../public/web5.png";
+import web3 from "../public/web3.png";
 import web6 from "../public/web6.PNG";
 import web7 from "../public/web7.png";
 import web8 from "../public/web8.png";
@@ -12,11 +12,18 @@ import Hoc from "./Hoc";
 
 const projects = [
   {
-    image: web8,
-    link: "https://life-plus-webapp.vercel.app/",
-    title: "Life Plus Web App",
+    image: web4,
+    link: "https://task-me-ai.vercel.app",
+    title: "Taskme AI",
     description:
-      "A comprehensive web application providing services for life coaching and personal development. Built with React and Tailwind CSS, offering a seamless user experience.",
+      "A project management tool to simplify your workflow with AI productivity partner. Break down project timelines into daily tasks with our AI project management solution."
+  },
+  {
+    image: web3,
+    link: "https://stream-vibe-ebon.vercel.app",
+    title: "StreamVibe Movie App",
+    description:
+      "A modern movie recommendation platform built with React and TMDB API.Features movie discovery, detailed information, trailers, and personalized recommendations with a sleek Netflix- inspired interface.",
   },
   {
     image: web9,
@@ -26,46 +33,39 @@ const projects = [
       "Created a language learning application optimized for a seamless learning experience in form of a game with microphone and 3D features using Assembly AI, threejs, etc.",
   },
   {
-    image: web1,
-    link: "https://store-clothing.vercel.app",
-    title: "Online Clothing Store",
+    image: web8,
+    link: "https://life-plus-webapp.vercel.app",
+    title: "Life Plus Web App",
     description:
-      "Developed an eCommerce store specializing in women's clothing. Features product browsing, cart management, and a smooth checkout process using Next.js and Timbu API.",
-  },
-  {
-    image: web4,
-    link: "https://bis-payer.vercel.app/",
-    title: "Taskme AI",
-    description:
-      "A project management tool to Simplify your Workflow with AI Productivity Partner. Break down project timelines into daily tasks with our AI project management solution."
-  },
-  {
-    image: web5,
-    link: "https://bulldozer-php.teams.hng.tech",
-    title: "Boilerplate",
-    description:
-      "Streamline your processes with a boilerplate built for efficiency and optimal productivity. Leverage pre-built sections to showcase your product effectively.",
-  },
-  {
-    image: web7,
-    link: "https://schoolbase-webapp.onrender.com/",
-    title: "School Base Web App",
-    description:
-      "A school management system that allows staff to handle scheduling, assignments, and student performance. Designed for simplicity and efficiency in educational institutions.",
+      "A comprehensive web application providing services for life coaching and personal development. Built with React and Tailwind CSS, offering a seamless user experience.",
   },
   {
     image: web6,
-    link: "https://match-lesson.vercel.app/",
+    link: "https://match-lesson.vercel.app",
     title: "Match Lesson Platform (WIP)",
     description:
       "An educational platform matching students with lessons in various subjects. Built to offer a personalized learning experience with a clean UI.",
   },
   {
     image: web2,
-    link: "https://simbi-tech-web-app.vercel.app/",
+    link: "https://simbi-tech-web-app.vercel.app",
     title: "Simbi Tech Web App",
     description:
-      "A web app for a tech company offering services in software development, consulting, and more. Focused on showcasing the company’s portfolio and expertise.",
+      "A web app to help techies learn, build and lead in tech. We provide tech and soft courses tailored to meet the needs of beginners and professionals to enhance their skills",
+  },
+  {
+    image: web1,
+    link: "https://store-clothing.vercel.app",
+    title: "Online Clothing Store",
+    description:
+      "Developed an eCommerce store specializing in women's clothing. Features product browsing, cart management, and a smooth checkout process.",
+  },
+  {
+    image: web7,
+    link: "https://schoolbase-webapp.onrender.com",
+    title: "School Base Web App",
+    description:
+      "A school management system that allows staff to handle scheduling, assignments, and student performance. Designed for simplicity and efficiency in educational institutions.",
   },
 ];
 
